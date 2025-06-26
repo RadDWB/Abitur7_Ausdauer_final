d209ceafaf887a273721147f13cb99800afb7830
RadDWB <github@oneonone.de>
2025-06-26 23:18:44 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import RundenzeitTabelle from '../components/RundenzeitTabelle';
 export default function HomePage() {
   return (
     <main className="p-4 max-w-3xl mx-auto space-y-6">
-      <h1 className="text-4xl font-bold text-center">Abiturprüfung: Zeitschätzlauf 5000 m</h1>
+      <h1 className="text-4xl font-bold text-center">Eingabeinterface</h1>
       <RundenzeitTabelle />
     </main>
   );
